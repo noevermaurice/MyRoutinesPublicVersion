f1c911738fef8ac02e83593bcc93f8a6e766816f
maurice Noever <momonoever@gmail.com>
2021-01-29 18:09:29 +0100

    views Deklartion and serach views

--- app/src/main/java/com/mn/myroutines/MainActivity.java
+++ app/src/main/java/com/mn/myroutines/MainActivity.java
@@ -3,12 +3,21 @@ package com.mn.myroutines;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    // Objects Deklartation
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews(){
+        // serach views with id
+        listView = findViewById(R.id.routinesList);
     }
 }
