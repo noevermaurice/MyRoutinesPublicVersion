2983b6b9a70b5aa1949e429d01532461daaf5203
maurice Noever <momonoever@gmail.com>
2021-01-30 00:03:30 +0100

    stand 29.01.2021 mit Dialog

--- app/src/main/java/com/mn/myroutines/AddAndEditRoutineActivity.java
+++ app/src/main/java/com/mn/myroutines/AddAndEditRoutineActivity.java
@@ -1,6 +1,7 @@
 package com.mn.myroutines;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.widget.EditText;
 import android.widget.ListView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
@@ -58,13 +60,30 @@ public class AddAndEditRoutineActivity extends AppCompatActivity {
         // listnen for Clicks
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int ListItemposition, long id) {
-                ListItemposition = listViewItemPosition;
-                routineSlotManager.setRoutineSlots();
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ShowDialog();
             }
         });
-
     }
+
+    private void ShowDialog() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.Action));
+        String[] actions = {getString(R.string.NoAction), getString(R.string.BluetoothOFF)};
+        builder.setItems(actions, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            routineSlotManager.setRoutineSlots(which);
+
+            }
+        });
+         AlertDialog dialog = builder.create();
+        dialog.show();
+
+
+        }
+    ;
 
     private void initManagers() {
         // inisalraztion for all Mangers in the App
--- app/src/main/java/com/mn/myroutines/RoutineSlotManager.java
+++ app/src/main/java/com/mn/myroutines/RoutineSlotManager.java
@@ -1,25 +1,34 @@
 package com.mn.myroutines;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class RoutineSlotManager {
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RoutineSlotManager  {
     // Objects Deklartation
     ListView listView;
     Context context;
     ListviewAdapterRoutine listviewAdapterRoutine;
     public String ArrayListSlotDefault;
     int listviewItemPosition;
-    public RoutineSlotManager(Context context,ListView listView, ListviewAdapterRoutine listviewAdapterRoutine, String ArrayListSlotDefault,int listviewItemPosition){
+    Activity activity = (Activity) context;
+
+
+    public RoutineSlotManager(Context context, ListView listView, ListviewAdapterRoutine listviewAdapterRoutine, String ArrayListSlotDefault, int listviewItemPosition) {
         //
         this.context = context;
         this.listView = listView;
         this.listviewAdapterRoutine = listviewAdapterRoutine;
         this.ArrayListSlotDefault = ArrayListSlotDefault;
-        this.listviewItemPosition= listviewItemPosition;
+        this.listviewItemPosition = listviewItemPosition;
     }
 
-    public void setDefaultSlots(){
+    public void setDefaultSlots() {
         // add Dedault array list
         listviewAdapterRoutine.arrayListSlots.add(ArrayListSlotDefault);
         listviewAdapterRoutine.arrayListSlots.add(ArrayListSlotDefault);
@@ -34,10 +43,19 @@ public class RoutineSlotManager {
         listView.setAdapter(listviewAdapterRoutine);
     }
 
-    public void setRoutineSlots(){
+    public void setRoutineSlots(int position) {
+       switch (position){
+           case 0:
 
+               break;
 
+           case 1:
+               Toast.makeText(context, "jj", Toast.LENGTH_SHORT).show();
+               break;
+       }
 
 
     }
+
+
 }
